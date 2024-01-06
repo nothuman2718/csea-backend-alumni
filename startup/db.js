886f2e4c537cb083a846773fc59a980c1687747f
nothuman2718 <nothuman2.718@gmail.com>
2024-01-06 10:43:42 +0530

    Update database connection in db.js

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const config = require("config");
 //Connect to mongoose
 module.exports = function () {
     mongoose
-        .connect(config.get("database") + "/csea-alumni-backend-finaltest2")
+        .connect(config.get("database"))
         .then(() => { console.log("Connected to mongoose") })
         .catch((err) => { console.log(err) })
 }
