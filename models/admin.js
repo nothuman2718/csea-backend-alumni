2dcaef0aa85d879722ffd0aebb25902930bb2877
nothuman2718 <nothuman2.718@gmail.com>
2024-01-06 09:48:11 +0530

    Update username validation in admin model

--- models/admin.js
+++ models/admin.js
@@ -12,7 +12,7 @@ const Admin = mongoose.model("Admin", adminSchema)
 
 function validateAdmin(admin) {
     const schema = Joi.object({
-        username: Joi.string().min(5).max(50).required(),
+        username: Joi.string().min(1).max(50).required(),
         password: Joi.string().min(6).max(255).required(),
         name: Joi.string().min(1).max(50).required()
     })
